f1fd1708ab96c0e4c981b38281f002dc33d2c2d6
marcotam <marcotam2002@gmail.com>
2024-03-31 17:24:02 +0800

    admin part

--- src/backend/adminhandler.js
+++ src/backend/adminhandler.js
@@ -6,3 +6,17 @@
  * Leung Ka Lun 1155157403
  * O Ching Lam 1155159131
  */
+
+const pool = require('./database');
+const AccountHandler = require('./accounthandler');
+
+class AdminHandler extends AccountHandler {
+  constructor() {
+    super();
+    // Additional initialization specific to admin handler
+  }
+
+  // Methods specific to admin handling can be defined here
+}
+
+module.exports = AdminHandler;
--- src/backend/userhandler.js
+++ src/backend/userhandler.js
@@ -6,3 +6,17 @@
  * Leung Ka Lun 1155157403
  * O Ching Lam 1155159131
  */
+
+const pool = require('./database');
+const AccountHandler = require('./accounthandler');
+
+class UserHandler extends AccountHandler {
+  constructor() {
+    super();
+    // Additional initialization specific to user handler
+  }
+
+  // Methods specific to user handling can be defined here
+}
+
+module.exports = UserHandler;
