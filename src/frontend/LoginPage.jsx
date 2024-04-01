f3fd220a6b8bea6c9f380128f70d56d3fc95fc35
kalunLL <tkalunleung@gmail.com>
2024-04-01 11:31:33 +0800

    login page starting

---
+++ src/frontend/LoginPage.jsx
@@ -0,0 +1,38 @@
+/*
+ * Group members:
+ * Tam King Man 1155160072
+ * Tsang Ho Yin 1155159307
+ * Ng Yuk Fung 1155176966
+ * Leung Ka Lun 1155157403
+ * O Ching Lam 1155159131
+ */
+
+import React from 'react';
+import './Login.css';
+
+const Login = () => {
+    return (
+        <div className="container">
+            <div className="left-column">
+                <img src="SoruIcon.png" alt="SORU Image" width="200" />
+                <h1>Soru</h1>
+                <p>Where Soul is Touched</p>
+            </div>
+            <div className="right-column">
+                <form>
+                    <h1>Login to see more</h1>
+                    <input type="text" placeholder="Username" required />
+                    <input type="password" placeholder="Password" required />
+                    <div className="forgot-password">
+                        <a href="#">Forgot Password</a>
+                    </div>
+                    <button className='submit-btn'>Submit</button>
+                </form>
+                <div className="line"></div>
+                <button className="register-btn">Register</button>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
