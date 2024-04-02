4c0f0e96af0153008ac061122d7df20379344f38
Ng Yuk Fung <53886955+ngyukfung@users.noreply.github.com>
2024-04-03 00:10:03 +0800

    Create Header for admin panel

---
+++ frontend/src/pages/AdminUserPanel.jsx
@@ -0,0 +1,27 @@
+/*
+ * Group members:
+ * Tam King Man 1155160072
+ * Tsang Ho Yin 1155159307
+ * Ng Yuk Fung 1155176966
+ * Leung Ka Lun 1155157403
+ * O Ching Lam 1155159131
+ */
+
+//todo: link "Soru" to main page
+import './AdminUserPanel.css';
+import "./format.css"
+function AdminUserPanel() {
+  return (
+    <div class="headerContainer">
+    <div class="leftHeader">
+        <h1><b>Soru</b></h1>
+        <h5>Admin Panel</h5>
+    </div>
+    <div class="rightHeader">
+        <h2>User Manager</h2>
+    </div>
+</div>
+  );
+}
+
+export default AdminUserPanel;
