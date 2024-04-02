15fc9e2c3daa832992f9e28da533f1728e810140
Andrewolf2002 <andrewtsang0629@gmail.com>
2024-04-02 09:30:15 +0800

    server-set-up

---
+++ backend/src/server.js
@@ -0,0 +1,25 @@
+/*
+ * Group members:
+ * Tam King Man 1155160072
+ * Tsang Ho Yin 1155159307
+ * Ng Yuk Fung 1155176966
+ * Leung Ka Lun 1155157403
+ * O Ching Lam 1155159131
+ */
+
+const express = require("express");
+const cors = require("cors");
+//const pool = require("./database")
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/test",async(req, res)=>{
+    res.json({message:"Hello!"});
+});
+
+
+app.listen(7000,()=>{
+    console.log("server started on localhost:7000");
+});
---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,40 @@
+/*
+ * Group members:
+ * Tam King Man 1155160072
+ * Tsang Ho Yin 1155159307
+ * Ng Yuk Fung 1155176966
+ * Leung Ka Lun 1155157403
+ * O Ching Lam 1155159131
+ */
+
+import React from 'react';
+import './LoginPage.css';
+import SoruIcon from '../assets/SoruIcon.png'
+import reactLogo from '../assets/react.svg'
+
+const Login = () => {
+    return (
+        <div className="container">
+            <div className="left-column">
+                <img src={SoruIcon} alt="SORU Image" width="200" />
+                <h1>Soru</h1>
+                <p>Where Soul is Touched</p>
+            </div>
+            <div className="right-column">
+                <form>
+                    <h1>Login to see more</h1>
+                    <input type="text" placeholder="Username" required />
+                    <input type="password" placeholder="Password" required />
+                    <div className="forgot-password">
+                        <a href="#">Forgot Password</a>
+                    </div>
+                    <button className='submit-btn'>Submit</button>
+                </form>
+                <div className="line"></div>
+                <button className="register-btn">Register</button>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
