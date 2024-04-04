a2d5f8238e24e2bad364e770275e1e83c4d1305c
Ng Yuk Fung <53886955+ngyukfung@users.noreply.github.com>
2024-04-05 00:37:22 +0800

    add post manager page; change user manager url

--- frontend/src/AppRoutes.jsx
+++ frontend/src/AppRoutes.jsx
@@ -3,14 +3,15 @@ import UserHomepage from "./pages/UserHomepage";
 import SearchPage from "./pages/SearchPage";
 import Login from "./pages/LoginPage"
 import AdminUserPanel from "./pages/AdminUserPanel";
-
+import AdminPostPanel from "./pages/AdminPostPanel";
 const AppRoutes=()=>{
     return(
         <Routes>
             <Route path="/" element = {<Login/>}/>
             <Route path="/search" element = {<SearchPage/>}/>
             <Route path="/homepage" element = {<UserHomepage/>}/>
-            <Route path="/admin/userlist" element = {<AdminUserPanel/>}/>
+            <Route path="/admin/usermanager" element = {<AdminUserPanel/>}/>
+            <Route path="/admin/postmanager" element = {<AdminPostPanel/>}/>  
         </Routes>
     );
 };
