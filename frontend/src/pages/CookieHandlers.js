8f011840156803d04a4a8980fc74ef4a987d4067
kalunLL <tkalunleung@gmail.com>
2024-04-04 21:57:35 +0800

    update names for the comment

--- frontend/src/pages/CookieHandlers.js
+++ frontend/src/pages/CookieHandlers.js
@@ -1,3 +1,26 @@
+/*
+ * Group members:
+ * Tam King Man 1155160072
+ * Tsang Ho Yin 1155159307
+ * Ng Yuk Fung 1155176966
+ * Leung Ka Lun 1155157403
+ * O Ching Lam 1155159131
+ */
+
+//create cookie for 30 days and delete cookie when unclick the checkbox
+export function handleCookie(event){
+    console.log(event.target.checked)
+    let username = document.getElementById("usernameInput").value;
+    let d = new Date();
+    if (event.target.checked){
+        d.setDate(d.getDate()+ 30);
+        document.cookie = 'username =' + username + "; expires=" + d +"; path=/"
+    }else{
+        d.setDate(d.getDate() - 1);
+        document.cookie = 'username =' + username + "; expires=" + d +"; path=/"
+    }   
+}
+
 //read from cookie
 export function getCookie(cname) {
     let name = cname + "=";
--- frontend/src/pages/LoginForm.jsx
+++ frontend/src/pages/LoginForm.jsx
@@ -1,3 +1,12 @@
+/*
+ * Group members:
+ * Tam King Man 1155160072
+ * Tsang Ho Yin 1155159307
+ * Ng Yuk Fung 1155176966
+ * Leung Ka Lun 1155157403
+ * O Ching Lam 1155159131
+ */
+
 import { React, useEffect, useState } from 'react';
 import './LoginForm.css'
 import { useNavigate } from 'react-router';
