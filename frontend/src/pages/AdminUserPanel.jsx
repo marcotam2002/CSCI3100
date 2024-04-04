2acde9e68071148c2ca3417acd84d0f8f304e171
Ng Yuk Fung <53886955+ngyukfung@users.noreply.github.com>
2024-04-04 14:31:50 +0800

    Update admin user panel

--- frontend/src/pages/AdminUserPanel.jsx
+++ frontend/src/pages/AdminUserPanel.jsx
@@ -15,7 +15,21 @@ import React from "react";
 import userIcon from "../assets/user.svg";
 import postIcon from "../assets/post.svg";
 
+/*Just for testing*/
+const testUsers = [
+  { id: "0000001", username: "FirstUser", password: "<PASSWORD>" },
+  { id: "0000002", username: "Name", password: "<PASSWORD>" },
+  { id: "0000003", username: "zerozeroTHREE", password: "<PASSWORD>" },
+  { id: "0000004", username: "IHATEPROJECT", password: "<PASSWORD>" },
+];
+
 function UserTable({ users }) {
+  const viewUser = (user) => {
+    window.confirm(`You shoulb be able to view ${user.username} profile.`)
+  }
+  const deleteUser = (user) => {
+    window.confirm(`You shoulb be able to delete ${user.username}.`)
+  }
   return (
     <table id="userTable">
       <tr>
@@ -29,8 +43,8 @@ function UserTable({ users }) {
           <td>{user.username}</td>
           <td>
             <div id="actionButtons">
-              <button>Edit</button>
-              <button id="deleteButton">Delete</button>
+            <button type="button" id="viewButton" onClick={()=>viewUser(user)}>View</button>
+              <button type="button" id="deleteButton" onClick={()=>deleteUser(user)}>Delete</button>
             </div>
           </td>
         </tr>
@@ -49,11 +63,13 @@ function AdminUserPanel() {
             image={userIcon}
             name={"User Manager"}
             color={"#1D67CD"}
+            func = {()=>alert("This should redirect to user manager.")}
           />
           <SideBarButton
             image={postIcon}
             name={"Post Manager"}
             color={"black"}
+            func = {()=>alert("This should redirect to post manager.")}
           />
         </div>
         <div id="main">
