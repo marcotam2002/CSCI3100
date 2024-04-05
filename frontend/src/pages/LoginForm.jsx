bb68039f2c1552a228d9fdf9c3ada1187a7e1e69
Andrewolf2002 <andrewtsang0629@gmail.com>
2024-04-05 11:25:12 +0800

    added server connection for login

--- frontend/src/pages/LoginForm.jsx
+++ frontend/src/pages/LoginForm.jsx
@@ -12,6 +12,8 @@ import './LoginForm.css'
 import RegistrationForm from './RegistrationForm';
 import { useNavigate } from 'react-router';
 
+const API_BASE_URL=import.meta.env.VITE_API_BASE_URL;
+
 function LoginForm() {
 
     const [state, setState] = useState(false);
@@ -33,33 +35,37 @@ function LoginForm() {
     //handler for fetching of the form
     const handleSubmit = async (event) => {
           event.preventDefault();
-          
           const data = {
               username: username,
               password: password
           };
+          //console.log(JSON.stringify(data))
+          //console.log(`${API_BASE_URL}/api/user`)
     
           // use POST method to send a request to the server
-          const response = await fetch('/', { 
+          const response = await fetch(`${API_BASE_URL}/api/user/login`, { 
             method: 'POST',
             headers: {
               'Content-Type': 'application/json'
             },
             body: JSON.stringify(data)
           });
-          if (response.status === 200){
+          if (response.status == 200){
               //successful login
+              console.log(response.body);
               const resdata = await response.json()
               document.cookie = "username="+ name;
               document.cookie = "userID" + resdata.userID
               document.cookie = "role="+ resdata.role;
               navigate('/homepage')
+              console.log("log in success")
           }else{
               //bad login: return error message
               console.log("Error")
               setPassword("");
               setUsername("");
           }
+          console.log(response);
     
     }
 
