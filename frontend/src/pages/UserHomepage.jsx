07fc3004c3b2b353b8b4b23e10c8ce3d3e788af6
Ng Yuk Fung <53886955+ngyukfung@users.noreply.github.com>
2024-04-06 02:06:00 +0800

    improve UI design for post page, home page, and button

--- frontend/src/pages/UserHomepage.jsx
+++ frontend/src/pages/UserHomepage.jsx
@@ -7,11 +7,13 @@
  * O Ching Lam 1155159131
  */
 
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { BsHeartFill, BsHeart, BsChat } from 'react-icons/bs';
+import likeIcon from '../assets/like.svg';
+import likedIcon from '../assets/liked.svg';
+import commentIcon from '../assets/comment.svg';
 import './format.css'
-import './UserHomepage.css'; 
+import './Post.css';
 
 function UserHomepage({ posts }) {
 
@@ -37,65 +39,63 @@ function UserHomepage({ posts }) {
       posts: updatedPosts,
     };
 
-    const response = await fetch('/post/change', { 
+    const response = await fetch('/post/change', {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(data)
     });
-    if (response.status === 200){
-        // successful update
-        console.log("successful update")
-    }else{
-        // failed update
-        console.log("failed to update")
+    if (response.status === 200) {
+      // successful update
+      console.log("successful update")
+    } else {
+      // failed update
+      console.log("failed to update")
     }
   };
-  
+
 
   const renderPost = (post) => {
     return (
       <div className="post-container " key={post.postID}>
-        <div className="post-subcontainer">
-          <div className="post-header">
-            <span className="post-username">{post.username}</span>
-            <span className="post-time">{post.time}</span>
-          </div>
-          <div className="post-description">
-            {post.description.split('\n').map((line, index) => (
-              (index < 3 || post.description.split('\n').length <= 3) && (
-                <p key={index}>{line}</p>
-              )
-            ))}
-          </div>
-          {post.description.split('\n').length > 3 && (
-            <div className="read-more">
-              <Link to={`/post/${post.postID}`}>Read More</Link>
-            </div>
-          )}
-          <div className="interaction-buttons">
-            <button className="like-button" onClick={(event) => ChangeLike(post.postID, event)}>
-              {post.liked ? <BsHeartFill style={{ color: 'red' }} /> : <BsHeart />}
-            </button>
-            <span className="like-count">{post.likes}</span>
-            <Link to={`/post/${post.postID}`} className="comment-button"><BsChat /></Link>
-            <span className="comment-count">{post.commentnum}</span>
-          </div>
-          <div className="comments">
-            {post.comments.slice(0, 2).map((comment, index) => (
-              <div className="comment" key={index}>
-                <span className="comment-username">{comment.username}</span>
-                <span className="comment-text">{comment.text}</span>
-              </div>
-            ))}
-          </div>
-          {post.comments.length > 2 && (
-            <div className="view-all-comments">
-              <Link to={`/post/${post.postID}`}>View all comments</Link>
-            </div>
-          )}
+        <div className="post-header">
+          <span className="post-username">{post.username}</span>
+          <span className="post-time">{post.time}</span>
         </div>
+        <div className="post-description">
+          {post.description.split('\n').map((line, index) => (
+            (index < 3 || post.description.split('\n').length <= 3) && (
+              <p key={index}>{line}</p>
+            )
+          ))}
+        </div>
+        {post.description.split('\n').length > 3 && (
+          <div className="read-more">
+            <Link to={`/post/${post.postID}`}>Read More</Link>
+          </div>
+        )}
+        <div className="interaction-buttons">
+          <button className="like-button" onClick={(event) => ChangeLike(post.postID, event)}>
+            {post.liked ? <img src={likedIcon} alt="liked" /> : <img src={likeIcon} alt="like" />}
+          </button>
+          <p>{post.likes}</p>
+          <Link to={`/post/${post.postID}`} className="comment-button"><img src={commentIcon} alt="comment" /></Link>
+          <p>{post.commentnum}</p>
+        </div>
+        <div className="comments">
+          {post.comments.slice(0, 2).map((comment, index) => (
+            <div className="comment" key={index}>
+              <span className="comment-username"><b>{comment.username}</b></span>
+              <span className="comment-text">{comment.text}</span>
+            </div>
+          ))}
+        </div>
+        {post.comments.length > 2 && (
+          <div className="view-all-comments">
+            <Link to={`/post/${post.postID}`}>View all comments</Link>
+          </div>
+        )}
       </div>
     );
   };
